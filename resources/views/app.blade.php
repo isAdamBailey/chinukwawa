438171a27366678ea400f099ff002db1070e0ca8
Adam Bailey <adamjbailey7@gmail.com>
2024-01-15 12:57:37 -0800

    add favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,6 +15,11 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet"/>
         <link href="https://fonts.bunny.net/css?family=oswald:200,400,700" rel="stylesheet"/>
 
+        <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
+        <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
+        <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
+        <link rel="manifest" href="{{ asset('site.webmanifest') }}">
+
         <!-- Scripts -->
         @routes
         @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
